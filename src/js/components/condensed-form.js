5aae0dc4b474b6dbfc80f454b30b0836aedabdf4
Tim Hammer <timh@govwizely.com>
2015-11-10 12:54:45 -0500

    Forms styling

--- src/js/components/condensed-form.js
+++ src/js/components/condensed-form.js
@@ -11,18 +11,21 @@ module.exports = React.createClass({
     return (
       <div>
         <div className="row">
-          <div className="col-md-3">
+          <div className="col-md-12">
             <Header cssClass="header-condensed" />
           </div>
+        </div>
 
-          <div className="col-md-4">
+        <div className="row">
+
+          <div className="col-md-4 keyword-input">
             <KeywordInput keyword={ this.props.keyword } onChange={ this.props.onKeywordChange } expanded={ false } />
           </div>
 
-          <div className="col-md-2">
+          <div className="col-md-3 category-input">
             <CountrySelect placeholder="Select Country" values={ this.props.countries } onChange={ this.props.onCountryChange } />
           </div>
-          <div className="col-md-2">
+          <div className="col-md-3 category-input">
             <IndustrySelect placeholder="Select Industry" values={ this.props.industries } onChange={ this.props.onIndustryChange }/>
           </div>
 
@@ -32,23 +35,25 @@ module.exports = React.createClass({
       
         <div className="row">
         
-            <div className="col-md-4 date-input">
-              <p className="text-muted">Expiration Date Start</p>
-              <ExpirationDatePicker onChange={ this.props.onExpirationDateStartChange } date={ this.props.expiration_date_start }/>
-            </div>
-            <div className="col-md-4 date-input">
-              <p className="text-muted">Expiration Date End</p>
-              <ExpirationDatePicker onChange={ this.props.onExpirationDateEndChange } date={ this.props.expiration_date_end }/>
-            </div>
-            <div className="col-md-2 search-button-col">
-              <span className="input-group-btn">
-                <button id="search-button" className={ 'btn btn-success btn-lg' } type="button" onClick={ this.props.onSubmit }>
-                  Search
-                </button>
-              </span>
-            </div>
+          <div className="col-md-2 date-input">
+            <p className="text-muted">Expiration Date Start</p>
+            <ExpirationDatePicker onChange={ this.props.onExpirationDateStartChange } date={ this.props.expiration_date_start }/>
+          </div>
+          <div className="col-md-2 date-input">
+            <p className="text-muted">Expiration Date End</p>
+            <ExpirationDatePicker onChange={ this.props.onExpirationDateEndChange } date={ this.props.expiration_date_end }/>
+          </div>
+          <div className="col-md-2 search-button-col">
+            <span className="input-group-btn">
+              <button id="search-button" className={ 'btn btn-success btn-lg' } type="button" onClick={ this.props.onSubmit }>
+                Search
+              </button>
+            </span>
+          </div>
         
         </div>
+
+
       </div>
     );
   }
--- src/js/components/expanded-form.js
+++ src/js/components/expanded-form.js
@@ -14,44 +14,44 @@ module.exports = React.createClass({
           <Header cssClass="text-center" />
         </div>
 
-        <div className="row">
-          <div className="col-md-6 keyword-input">
+        <div className="row expanded-row">
+          <div className="col-md-12 keyword-input">
             <p className="text-muted">Search by Keyword</p>
             <KeywordInput keyword={ this.props.keyword } onChange={ this.props.onKeywordChange } />
           </div>
+        </div>
 
+        <div className="row expanded-row">
           <div className="col-md-6 category-input">
-            <p className="text-muted">Search by Category</p>
-            <div className="col-md-6">
-              <CountrySelect onChange={ this.props.onCountryChange }  placeholder="Select Country"  />
-            </div>
-            <div className="col-md-6">
-              <IndustrySelect onChange={ this.props.onIndustryChange } placeholder="Select Industry"  />
-            </div>
- 
+            <p className="text-muted">Search by Country</p>
+            <CountrySelect onChange={ this.props.onCountryChange }  placeholder="Select Country"  />
+          </div>
+          <div className="col-md-6 category-input">
+            <p className="text-muted">Search by Industry</p>
+            <IndustrySelect onChange={ this.props.onIndustryChange } placeholder="Select Industry"  />
           </div>
         </div>
 
-        <div className="row expanded-bottom-row">
+        <div className="row expanded-row expanded-bottom-row">
+          <div className="col-md-4 date-input">
+            <p className="text-muted"> Expiration Date Start </p>
+            <ExpirationDatePicker onChange={ this.props.onExpirationDateStartChange } />
+          </div>
 
-            <div className="col-md-2 date-input">
-              <p className="text-muted"> Expiration Date Start </p>
-              <ExpirationDatePicker onChange={ this.props.onExpirationDateStartChange } />
-            </div>
+          <div className="col-md-4 date-input">
+            <p className="text-muted"> Expiration Date End </p>
+            <ExpirationDatePicker onChange={ this.props.onExpirationDateEndChange } />
+          </div>
 
-            <div className="col-md-2 date-input">
-              <p className="text-muted"> Expiration Date End </p>
-              <ExpirationDatePicker onChange={ this.props.onExpirationDateEndChange } />
-            </div>
-
-            <div className="col-md-2 search-button-col">
-              <span className="input-group-btn">
-                <button id="search-button" className={ 'btn btn-success btn-lg' } type="button" onClick={ this.props.onSubmit }>
-                  Search
-                </button>
-              </span>
-            </div>
+          <div className="col-md-4 search-button-col">
+            <span className="input-group-btn">
+              <button id="search-button" className={ 'btn btn-success btn-lg' } type="button" onClick={ this.props.onSubmit }>
+                Search
+              </button>
+            </span>
+          </div>
         </div>
+
       </div>
     );
   }
